ba35779337b27a58d7128e06dec5e4870fa6d5c0
Vojtěch Dolák <dolakv.05@spst.cz>
2022-02-10 10:48:47 +0100

    Hotovo

--- P01/Form1.cs
+++ P01/Form1.cs
@@ -19,12 +19,61 @@ namespace P01
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string pismena = "QWERTYUIOPASDFGHJKLZXCVBNM .,?!";
+            int i = 0;
+            string pismena = " !?.,MNBVCXZLKJHGFDSAPOIUYTREWQ";
             foreach (Control ctrl in panel1.Controls)
             {
                 if (ctrl is Button) (ctrl as Button).Text = pismena[i].ToString();
                 i++;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char pismeno = Convert.ToChar((sender as Button).Text);
+            textBox1.Text += pismeno;
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            string novepismeno = "";
+            foreach (char pismeno in textBox1.Text)
+            {
+                if (pismeno >= 'A')
+                {
+                    if (pismeno <= 'W')
+                    {
+                        novepismeno += Convert.ToChar(pismeno + 3);
+                    }
+                    else
+                    {
+                        novepismeno += Convert.ToChar(pismeno - 23);
+                    }
+                }
+
+            }
+            textBox1.Text = novepismeno;
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            string novepismeno = "";
+            foreach (char pismeno in textBox1.Text)
+            {
+                if (pismeno >= 'A')
+                {
+                    if (pismeno <= 'W')
+                    {
+                        novepismeno += Convert.ToChar(pismeno - 3);
+                    }
+                    else
+                    {
+                        novepismeno += Convert.ToChar(pismeno + 23);
+                    }
+                }
+
+            }
+            textBox1.Text = novepismeno;
         }
     }
 }
